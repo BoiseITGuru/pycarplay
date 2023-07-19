bcb2e5c661e6f8d6f945bebddc5b2f8380c2c976
psantillan <paul.r.santillan@gmail.com>
2023-07-18 20:27:15 -0700

    updated HW accel.

--- decoder.py
+++ decoder.py
@@ -45,8 +45,11 @@ class Decoder:
 		# Here we added the -c:v h264_mmal to utilize hardware acceleration
 		# for h264 decoding on Raspberry Pi
 		# OLD: self.child = subprocess.Popen(["ffmpeg", "-threads", "4", "-i", "-", "-vf", "fps=7", "-c:v", "png", "-f", "image2pipe", "-"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, bufsize=1)
+		# self.child = subprocess.Popen(
+		# 	["ffmpeg", "-threads", "4", "-c:v", "h264_mmal", "-i", "-", "-vf", "fps=7", "-c:v", "png", "-f",
+		# 	 "image2pipe", "-"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, bufsize=1)
 		self.child = subprocess.Popen(
-			["ffmpeg", "-threads", "4", "-c:v", "h264_mmal", "-i", "-", "-vf", "fps=7", "-c:v", "png", "-f",
+			["ffmpeg", "-hwaccel", "auto", "-threads", "4", "-i", "-", "-vf", "fps=7", "-c:v", "png", "-f",
 			 "image2pipe", "-"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, bufsize=1)
 		fd = self.child.stdout.fileno()
 		fl = fcntl.fcntl(fd, fcntl.F_GETFL)
